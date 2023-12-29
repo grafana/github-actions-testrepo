6e21835e5b3a1064b21413254fd38c6b2c8fef6f
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2023-12-29 15:09:17 +0100

    inputFileContents

--- .github/workflows/scripts/feature-toggle-cleanup/feature-toggle-cleanup.mjs
+++ .github/workflows/scripts/feature-toggle-cleanup/feature-toggle-cleanup.mjs
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 export default function printStuff() {
 	const inputFileContents = fs.readFileSync(process.env.FEATURE_TOGGLES_CSV_FILE_PATH);
-	const records = parse(input, {
+	const records = parse(inputFileContents, {
 		columns: true,
 		skip_empty_lines: true
 	  });
