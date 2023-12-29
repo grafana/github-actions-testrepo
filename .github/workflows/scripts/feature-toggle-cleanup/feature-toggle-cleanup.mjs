296ee426694d1f3a5bc273e2073fac291de5fa03
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2023-12-29 15:18:12 +0100

    adding date check logic

--- .github/workflows/scripts/feature-toggle-cleanup/feature-toggle-cleanup.mjs
+++ .github/workflows/scripts/feature-toggle-cleanup/feature-toggle-cleanup.mjs
@@ -1,11 +1,36 @@
 import { parse } from 'csv-parse/sync';
 import fs from 'fs';
 
-export default function printStuff() {
+/***
+ * Feauture Flag Structure example
+ *  Name: 'disableEnvelopeEncryption',
+    Stage: 'GA',
+    Owner: '@grafana/grafana-as-code',
+    Created: '2022-05-24',
+    requiresDevMode: 'false',
+    RequiresLicense: 'false',
+    RequiresRestart: 'false',
+    FrontendOnly: 'false'
+ * 
+ */
+
+
+export default function cleanupFeatureFlags() {
+	const today = new Date();
 	const inputFileContents = fs.readFileSync(process.env.FEATURE_TOGGLES_CSV_FILE_PATH);
-	const records = parse(inputFileContents, {
+	const parsedFeatureFlags = parse(inputFileContents, {
 		columns: true,
-		skip_empty_lines: true
+		skip_empty_lines: true,
+		cast: true,
+		cast_date: true,
 	  });
-	console.log(records);
+
+	for (const flag of parsedFeatureFlags) {
+		console.log(flag);
+		if (flag.Created > today.setMonth(today.getMonth() - 6)) {
+			console.log(`The flag ${flag.Name} was created more than 6 months ago. It should be checked.`);
+		}
+	}
+	
+
 }
